e426ebee4446b275ba79705c533443a4c3f2b638
Liviu <liviubiur@gmail.com>
2020-07-29 12:49:35 +0200

    Update EmployeeNotFoundAdvice.java

--- src/main/java/com/liviubiur/payrollservice/employee/EmployeeNotFoundAdvice.java
+++ src/main/java/com/liviubiur/payrollservice/employee/EmployeeNotFoundAdvice.java
@@ -1,10 +1,10 @@
 package com.liviubiur.payrollservice.employee;
 
-    import org.springframework.http.HttpStatus;
-    import org.springframework.web.bind.annotation.ControllerAdvice;
-    import org.springframework.web.bind.annotation.ExceptionHandler;
-    import org.springframework.web.bind.annotation.ResponseBody;
-    import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class EmployeeNotFoundAdvice {
